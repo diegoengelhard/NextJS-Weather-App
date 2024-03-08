33b23a0789d18b4103b6c2a80d3ce2b472b974b7
Diego Engelhard <00356119@uca.edu.sv>
2024-03-08 10:33:23 -0600

    refactor: project files

--- next-weather-app/src/apollo-client.ts
+++ next-weather-app/src/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 // Create a new Apollo Client
-export const createClient = () => {
+export const getClient = () => {
     const client = new ApolloClient({
         uri: process.env.APOLLO_URI,
         cache: new InMemoryCache(),
@@ -12,3 +12,6 @@ export const createClient = () => {
 
     return client;
 }
+
+// Uri: https://dashboard.stepzen.com/explorer?endpoint=api%2Fweather
+// Key: wackersdorf::stepzen.io+1000::961b35cdc4d68e45c859eb2410511fd593090e7aff819deee1637f679ace622b
--- next-weather-app/src/app/(pages)/location/[city]/[lat]/[long]/page.tsx
+++ next-weather-app/src/app/(pages)/location/[city]/[lat]/[long]/page.tsx
@@ -1,5 +1,7 @@
 'use client';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getClient } from '@/apollo-client';
+import FETCH_WEATHER from '@/graphql/queries/fetchWeather.queries';
 
 // Define params props
 type Props = {
@@ -11,6 +13,40 @@ type Props = {
 }
 
 const WeatherPage = ({ params: { city, lat, long } }: Props) => {
+    const client = getClient();
+
+    const [weatherData, setWeatherData] = useState<Root | null>(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await client.query({
+                    query: FETCH_WEATHER,
+                    variables: {
+                        latitude: lat,
+                        longitude: long,
+                        timezone: 'GMT'
+                    }
+                });
+                console.log(response);
+                if (!response || !response.data) {
+                    throw new Error('No data returned from query');
+                }
+
+                const result: Root = response.data.myQuery;
+                console.log(result);
+
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, [lat, long]);
+
     return (
         <div>Welcome to {city} </div>
     )

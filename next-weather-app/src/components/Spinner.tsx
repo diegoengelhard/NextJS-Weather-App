8e6deaca509348c2d30ba2c8386f5aced2cdfb9b
Diego Engelhard <00356119@uca.edu.sv>
2024-03-12 10:45:45 -0600

    style: update spinner

--- next-weather-app/src/components/Spinner.tsx
+++ next-weather-app/src/components/Spinner.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { FadeLoader } from 'react-spinners';
+import { Subtitle, Text } from "@tremor/react";
 
 const Spinner = () => {
   return (
-    <div className='fixed top-0 left-0 right-0 bottom-0 bg-gradient-to-br from-[#394F68] to-[#183B7E] text-white flex items-center justify-center'>
+    <div className='flex flex-col fixed top-0 left-0 right-0 bottom-0 bg-gradient-to-br from-[#394F68] to-[#183B7E] text-white items-center justify-center'>
       <FadeLoader color="#f1ffb8" />
+      <Subtitle className="text-xl text-center">
+        Loading City Weather Information
+      </Subtitle>
     </div>
   )
 }
